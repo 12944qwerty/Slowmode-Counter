3b916c83556091b0dfa72734dcb053b7adde3f86
GriefMoDz <griefmodz@gmail.com>
2022-02-09 17:10:31 +1100

    [v2.0.2] Minor adjustments and fixes

--- index.jsx
+++ index.jsx
@@ -1,10 +1,12 @@
-const { React, getModule, getModuleByDisplayName, i18n: { Messages } } = require('powercord/webpack');
+const { React, getModule, getModuleByDisplayName, constants: { Durations }, i18n: { Messages } } = require('powercord/webpack');
 const { inject, uninject } = require('powercord/injector');
 const { findInReactTree } = require('powercord/util');
 const { Plugin } = require('powercord/entities');
 
 const SlowmodeStore = require('./lib/Store');
 const ErrorBoundary = require('./components/ErrorBoundary');
+
+const moment = getModule([ 'momentProperties' ], false);
 
 module.exports = class SlowmodeCounter extends Plugin {
 	constructor () {
@@ -23,28 +25,32 @@ module.exports = class SlowmodeCounter extends Plugin {
 		const { SlowmodeType } = getModule([ 'SlowmodeType' ], false);
 		const { useStateFromStores } = getModule([ 'useStateFromStores' ], false);
 
-		const SlowmodeCooldown = ({ channel, isThreadCreation }) => {
+		const SlowmodeCooldown = ({ channel, isThreadCreation, renderCounterOnly }) => {
 			const { slowmodeCooldownGuess } = useStateFromStores([ SlowmodeStore ], () => ({
 				slowmodeCooldownGuess: SlowmodeStore.getSlowmodeCooldownGuess(channel.id, isThreadCreation ? SlowmodeType.CreateThread : SlowmodeType.SendMessage)
 			}));
 
-			return `${Messages.CHANNEL_SLOWMODE_DESC_IMMUNE}${slowmodeCooldownGuess > 0 ? ` ${this.millisecondsToReadableFormat(slowmodeCooldownGuess)}` : ''}`;
+			return `${renderCounterOnly ? Messages.CHANNEL_SLOWMODE_DESC_IMMUNE : ''}${slowmodeCooldownGuess > 0 ? ` ${this.millisecondsToReadableFormat(slowmodeCooldownGuess)}` : ''}`;
 		};
 
 		const TypingUsers = getModuleByDisplayName('FluxContainer(TypingUsers)', false).prototype.render.call({ memoizedGetStateFromStores: () => ({}) }).type;
 		if (!TypingUsers) return this.error('Missing “TypingUsers” component - skipping injection');
 
 		inject('force-slowmode-timer', TypingUsers.prototype, 'render', function (_, res) {
-			const tooltip = findInReactTree(res, n => typeof n.children === 'function');
-			if (!tooltip) return res;
+			const { channel, isBypassSlowmode, isThreadCreation } = this.props;
 
-			const oldMethod = tooltip.children;
-			tooltip.children = (props) => {
-				const res = oldMethod(props);
-				if (res.props.children[0] !== Messages.CHANNEL_SLOWMODE_DESC_IMMUNE) return res;
+			if (channel.rateLimitPerUser === 0 || !isBypassSlowmode) return res;
+
+			const Tooltip = findInReactTree(res, n => typeof n.children === 'function');
+			if (!Tooltip) return res;
+
+			const { children } = Tooltip;
+
+			Tooltip.children = (props) => {
+				const res = children(props);
 
 				res.props.children[0] = <ErrorBoundary main={_this}>
-					<SlowmodeCooldown channel={this.props.channel} isThreadCreation={this.props.isThreadCreation} />
+					<SlowmodeCooldown channel={channel} isThreadCreation={isThreadCreation} renderCounterOnly={res.props.children[0] === Messages.CHANNEL_SLOWMODE_DESC_IMMUNE} />
 				</ErrorBoundary>;
 
 				return res;
@@ -55,15 +61,16 @@ module.exports = class SlowmodeCounter extends Plugin {
 	}
 
 	millisecondsToReadableFormat (ms) {
-		let seconds = Math.floor((ms / 1000) % 60),
-			minutes = Math.floor((ms / (1000 * 60)) % 60),
-			hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
+		const duration = moment.duration(ms);
+		const seconds = String(duration.seconds()).padStart(2, '0');
 
-		hours = (hours < 10) ? hours < 1 ? '' : '0' + hours + ':': hours + ':';
-		minutes = (minutes < 10) ? '0' + minutes : minutes;
-		seconds = (seconds < 10) ? '0' + seconds : seconds;
+		if (ms > 1e3 * Durations.HOUR) {
+			const minutes = String(duration.minutes()).padStart(2, '0')
 
-		return hours + minutes + ':' + seconds;
+			return duration.hours() + ':' + minutes + ':' + seconds;
+		} else {
+			return duration.minutes() + ':' + seconds;
+		}
 	}
 
 	pluginWillUnload () {
